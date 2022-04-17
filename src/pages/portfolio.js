8966239dc38b1de6f695d4df6e32682b3dc55be5
Adrian Gajda <adrian.gajda@hotmail.com>
2022-04-17 21:31:13 +0100

    portfolio landing page updates

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -9,14 +9,56 @@ export default function Home() {
   return (
     <div>
       <Navbar></Navbar>
-      <div className="bg-teal-100 w-screen h-screen flex justify-center items-center max-w-screen-2xl mx-auto">
-        <div className="grid grid-cols-3">
-          <Link to="/portfolio/crosley">Crosley Radio</Link>
-          <Link to="/portfolio/lasgo">LASGO</Link>
-          <Link to="/portfolio/kathleen">Kathleen</Link>
-          <Link to="/portfolio/banabay">Banabay</Link>
-          <Link to="/portfolio/adlink">Adlink</Link>
-          <Link to="/portfolio/bluesenses">BluesenseS</Link>
+      <div className=" w-screen h-screen flex justify-center items-center max-w-screen-2xl mx-auto py-8">
+        <div className="grid grid-cols-3 gap-8">
+          <Link to="/portfolio/crosley">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
+          <Link to="/portfolio/lasgo">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
+          <Link to="/portfolio/kathleen">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
+          <Link to="/portfolio/banabay">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
+          <Link to="/portfolio/adlink">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
+          <Link to="/portfolio/bluesenses">
+            {" "}
+            <StaticImage
+              src="../images/logo.webp"
+              width={300}
+              height={300}
+            ></StaticImage>
+          </Link>
         </div>
       </div>
     </div>
