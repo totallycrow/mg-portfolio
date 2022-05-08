d8164fbb0a5e19d7b9aae83f066bfab3d4088026
Adrian Gajda <adrian.gajda@hotmail.com>
2022-05-08 20:11:54 +0100

    add mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,21 @@
 import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { useState, useEffect } from "react"
+import { useScrollRestoration } from "gatsby"
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    open
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "visible")
+  }, [open])
+
+  const hidden = " hidden"
+  const visible = " visible"
+
   return (
     <nav className="border-b-2">
       <div className="mx-auto px-4 h-14 flex justify-between items-center  max-w-screen-2xl">
@@ -16,7 +29,22 @@ export default function Navbar() {
           </Link>
         </div>
 
-        <div className="px-10">
+        <div>
+          <div
+            class="space-y-2"
+            onClick={() => {
+              console.log("state before click:", open)
+              setOpen(!open)
+              console.log("state:", open)
+            }}
+          >
+            <span class="block w-8 h-0.5 bg-gray-600"></span>
+            <span class="block w-8 h-0.5 bg-gray-600"></span>
+            <span class="block w-5 h-0.5 bg-gray-600"></span>
+          </div>
+        </div>
+
+        {/* <div className="px-10">
           <Link to="/about-me" className="px-4">
             About
           </Link>
@@ -26,6 +54,55 @@ export default function Navbar() {
           <Link to="/contact" className="px-4">
             Contact Me
           </Link>
+        </div> */}
+      </div>
+      {/* "w-60 h-full shadow-md bg-white px-1 absolute top-0 left-0 z-50" */}
+      <div
+        class={
+          open === false
+            ? "hidden"
+            : "visible w-48 h-full shadow-md bg-white px-1 absolute top-0 left-0 z-50"
+        }
+      >
+        {/* <ul class="relative">
+          <li class="relative">
+            <a
+              class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              href="#!"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="dark"
+            >
+              Sidenav link 1
+            </a>
+          </li>
+          <li class="relative">
+            <a
+              class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              href="#!"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="dark"
+            >
+              Sidenav link 2
+            </a>
+          </li>
+          <li class="relative">
+            <a
+              class="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-gray-700 text-ellipsis whitespace-nowrap rounded hover:text-gray-900 hover:bg-gray-100 transition duration-300 ease-in-out"
+              href="#!"
+              data-mdb-ripple="true"
+              data-mdb-ripple-color="dark"
+            >
+              Sidenav link 2
+            </a>
+          </li>
+        </ul> */}
+
+        <div className="w-full h-full flex items-start justify-start max-auto">
+          <div className="pl-10 pt-40">
+            <div>test</div>
+            <div>test</div>
+            <div>test</div>
+          </div>
         </div>
       </div>
     </nav>
