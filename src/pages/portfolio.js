21846f8fa66d7def0e890d0e319757622a803686
Adrian Gajda <adrian.gajda@hotmail.com>
2022-05-21 19:54:51 +0100

    style portfolio card

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,17 +10,96 @@ import { StaticImage } from "gatsby-plugin-image"
 export default function Home() {
   return (
     <Layout>
-      <div className="bg-teal-100 flex justify-center items-center max-w-screen-2xl mx-auto py-8">
-        <div className="bg-blue-100 grid grid-cols-3 gap-8">
-          <Link to="/portfolio/crosley">
-            {" "}
-            <StaticImage
-              src="../images/logo.webp"
-              width={300}
-              height={300}
-            ></StaticImage>
-          </Link>
-          <Link to="/portfolio/lasgo">
+      {/* <div className="bg-teal-100 flex justify-center items-center max-w-screen-2xl mx-auto py-8"> */}
+      <h1 className="text-2xl pl-2 mt-4">Work | Projects</h1>
+      <div className="">
+        {/* <div className="bg-blue-100 grid grid-cols-3 gap-8"> */}
+        <div className="text-center ">
+          {/* <div className="h-80 p-4 m-2">
+            <Link to="/portfolio/crosley">
+              {" "}
+              <StaticImage
+                src="../images/logo.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div> */}
+
+          <div className="h-80 p-4 m-2 drop-shadow-md rounded-lg">
+            <Link to="/portfolio/lasgo">
+              {" "}
+              <StaticImage
+                src="../images/lasgo-logo-pf.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div>
+
+          <div className="h-80 p-4 m-2">
+            <Link to="/portfolio/kathleen">
+              {" "}
+              <StaticImage
+                src="../images/logo.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div>
+
+          <div className="h-80 p-4 m-2">
+            <Link to="/portfolio/banabay">
+              {" "}
+              <StaticImage
+                src="../images/logo.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div>
+
+          <div className="h-80 p-4 m-2">
+            <Link to="/portfolio/adlink">
+              {" "}
+              <StaticImage
+                src="../images/logo.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div>
+
+          <div className="h-80 p-4 m-2">
+            <Link to="/portfolio/bluesenses">
+              {" "}
+              <StaticImage
+                src="../images/logo.webp"
+                className="h-full"
+                objectPosition={""}
+                objectFit={"cover"}
+                // width={300}
+                // height={300}
+              ></StaticImage>
+            </Link>
+          </div>
+
+          {/* <Link to="/portfolio/lasgo">
             {" "}
             <StaticImage
               src="../images/logo.webp"
@@ -59,7 +138,7 @@ export default function Home() {
               width={300}
               height={300}
             ></StaticImage>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </Layout>
